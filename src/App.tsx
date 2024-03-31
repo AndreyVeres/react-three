7f25f53901415b91e8be9cb50a7dbc9596bd4928
Andrey Veres <spacecmetanka@gmail.com>
2024-03-31 21:21:27 +0300

    suspnce

--- src/App.tsx
+++ src/App.tsx
@@ -6,19 +6,22 @@ import { BuildPage } from './pages/car';
 import { GhostPage } from './pages/Ghost';
 import { PortalPage } from './pages/Portal';
 import { MedievalPage } from './pages/medieval/page';
+import { Suspense } from 'react';
 
 function App() {
   return (
     <>
       <Nav />
       <Routes>
-        <Route path='/' element={<Navigate to={'/build'} />} />
-        <Route path='/land' element={<Land />} />
-        <Route path='/space' element={<Space />} />
-        <Route path='/build' element={<BuildPage />} />
-        <Route path='/ghost' element={<GhostPage />} />
-        <Route path='/portal' element={<PortalPage/>} />
-        <Route path='/med' element={<MedievalPage/>}/>
+        <Suspense fallback={null}>
+          <Route path='/' element={<Navigate to={'/build'} />} />
+          <Route path='/land' element={<Land />} />
+          <Route path='/space' element={<Space />} />
+          <Route path='/build' element={<BuildPage />} />
+          <Route path='/ghost' element={<GhostPage />} />
+          <Route path='/portal' element={<PortalPage />} />
+          <Route path='/med' element={<MedievalPage />} />
+        </Suspense>
       </Routes>
     </>
   );
